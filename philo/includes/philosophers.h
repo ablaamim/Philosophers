7c160a8131cf9596b25f97b5269531a7ad701e76
ablaamim <aku-no-hana@outlook.fr>
2022-08-01 15:50:47 +0100

    letsgooooo

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -5,72 +5,17 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/13 14:28:03 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/07/15 15:56:00 by ablaamim         ###   ########.fr       */
+/*   Created: 2022/08/01 14:03:24 by ablaamim          #+#    #+#             */
+/*   Updated: 2022/08/01 14:10:09 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#ifndef PHILO_H
+# define PHILO_H
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <pthread.h>
-# include <stdbool.h>
-
-/*
- * Each philosopher state.
-*/
-
-# define HAS_TAKEN_A_FORK 1
-# define IS_EATING 2
-# define IS_SLEEPING 3
-# define IS_THINKING 4
-# define DIED 5
-
-typedef struct s_data
-{
-	int	philo_is_alone;
-	int	number_of_philosophers;
-	int	dinner_is_over;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat;
-}	t_data;
-
-typedef struct s_philo
-{
-	int			name;
-	int			meals;
-	long		last_meal;
-	pthread_t	thread;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-	pthread_mutex_t	*lock_meal;
-	pthread_mutex_t	*lock_meals;
-}	t_philo;
-
-/*
- * Entry point of my program.
- * Core functions defining the algos.
-*/
-
-int		main(int argc, char **argv);
-void	philosophers_parser(int argc, char **argv);
-
-/*
- * Parser utils :
-*/
-
-int	ft_isdigit(int c);
-int	ft_atoi(char *str);
-
-/*
- * Init data :
-*/
-
-void	initialize_data(int argc, char **argv, t_data *data);
 
 #endif
--- philo/sources/main.c
+++ philo/sources/main.c
@@ -5,27 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/12 15:30:04 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/07/13 21:39:41 by ablaamim         ###   ########.fr       */
+/*   Created: 2022/08/01 15:26:26 by ablaamim          #+#    #+#             */
+/*   Updated: 2022/08/01 15:27:21 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 
-/*
- * Entry point of my program.
- *
- * --> All core functions are called in here.
- */
-
 int	main(int argc, char **argv)
- {
-	t_data			data;
-	pthread_mutex_t	*forks;
-	t_philo		*philos;
+{
 
-	philosophers_parser(argc, argv);
-	initialize_data(argc, argv, &data);
-	initialize_forks(data.number_of_philosophers, &data, &forks, &philos);
 	return (EXIT_SUCCESS);
 }
